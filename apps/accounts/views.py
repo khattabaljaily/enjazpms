0e4543fa87c1f414d966d9cc8c3290c67c1824b0
Khattab Aljaily <khattabaljaily@gmail.com>
2026-05-01 12:16:29 +0300

    fix error

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -19,11 +19,7 @@ from .forms import Step1UserForm, Step2BusinessForm, Step3SettingsForm, LoginFor
 
 
 def _wants_json(request):
-    return (
-        getattr(request, 'is_api', False)
-        or request.headers.get('x-requested-with') == 'XMLHttpRequest'
-        or 'application/json' in request.headers.get('Accept', '')
-    )
+    return getattr(request, 'is_api', False)
 
 
 def _serialize_form_errors(form):
