2feec27db30c30b1f1c977a46cbb3843c03f9248
Khattab Aljaily <khattabaljaily@gmail.com>
2026-06-08 17:57:48 +0300

    fix reverse import in tenant email notification

--- apps/core/views.py
+++ apps/core/views.py
@@ -1740,8 +1740,7 @@ def tenant_create_api(request):
         from django.template.loader import render_to_string
         from django.utils import timezone
 
-        from django.urls import reverse as _reverse
-        dashboard_url = request.build_absolute_uri(_reverse('core:tenant_list'))
+        dashboard_url = request.build_absolute_uri('/tenants/')
         html_body = render_to_string('core/email/new_tenant_notification.html', {
             'tenant': tenant,
             'admin_full_name': full_name,
